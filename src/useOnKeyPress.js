3c5adf3722ece4eaa915ce1c5fd5d33927ecf8af
FANTRASTIC <saugatpariyar79@gmail.com>
2023-09-04 16:23:52 +0545

    Commiting bingovie

---
+++ src/useOnKeyPress.js
@@ -0,0 +1,16 @@
+import {useEffect} from 'react'
+
+export const useOnKeyPress = (callback, targetKey) => {
+    useEffect(() => {
+
+        const KeyPressHandler = (event)=> {
+            if(event.key === targetKey) {
+                callback();
+            }
+        }
+        window.addEventListener('keydown', KeyPressHandler);
+        return()=>{
+            window.removeEventListener('keydown', KeyPressHandler);
+        };
+    }, [callback, targetKey]);
+}
